f47bf80d015989694ace66aff7eed2358b93ea84
Alessandro Fornasier <4l3.fornasier@gmail.com>
2025-05-02 10:08:54 +0200

    Added comment

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,4 @@
+# Pull pytorch docker: docker pull pytorch/pytorch
+
 xhost +
 docker run --net=host -it --runtime=nvidia --gpus all -e NVIDIA_VISIBLE_DEVICES=all --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume "$(pwd):/workspace" --workdir /workspace pytorch/pytorch:latest
